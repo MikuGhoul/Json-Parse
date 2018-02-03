0f57badf5b573afef46b1c7558d02a3e1ad2e19a
MikuGhoul <mikughoull@gmail.com>
2018-02-03 23:56:46 +0800

    add test unit

--- leptjson.c
+++ leptjson.c
@@ -45,3 +45,8 @@ int lept_parse(lept_value* v, const char* json) {
     lept_parse_whitespace(&c);
     return lept_parse_value(&c, v);
 }
+
+lept_type lept_get_type(const lept_value* v) {
+    assert(v != NULL);
+    return v->type;
+}
